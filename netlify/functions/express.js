2ae3eb8a3f22407a82303c7f6840127f1749cb48
khushalsinhmar <shimarsabb@gmail.com>
2025-03-04 18:21:59 +0530

    server.js

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -1,4 +1,5 @@
+const ejs = require('ejs'); // Force inclusion of EJS
 const serverless = require('serverless-http');
-const app = require('../../app');  // adjust the path as needed
+const app = require('../../app'); // adjust path as needed
 
 module.exports.handler = serverless(app);
